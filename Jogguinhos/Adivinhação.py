0ea9551064e5dd458746825c801dea9b20eee575
dvgba <72717322+dvgba@users.noreply.github.com>
2022-11-13 06:03:39 -0300

    Domigo

---
+++ Jogguinhos/Adivinhação.py
@@ -0,0 +1,8 @@
+# --Importar da biblioteca random a funação que pega uma numero aleatorio
+from random import randint 
+# -- Inputs
+
+seu_nome = input("Olá, qual o seu nome? ")
+print("Okay! %s, eu escolhendo um número de 1 até 10. Você consegue adivinhar?")
+
+
