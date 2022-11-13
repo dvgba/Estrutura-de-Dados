5c4e2e96e0ceaff3f5dd10173b4df210979d0bdf
dvgba <72717322+dvgba@users.noreply.github.com>
2022-11-13 06:25:44 -0300

    625

--- Jogguinhos/Adivinhação.py
+++ Jogguinhos/Adivinhação.py
@@ -1,8 +1,21 @@
-# --Importar da biblioteca random a funação que pega uma numero aleatorio
 from random import randint 
-# -- Inputs
-
 seu_nome = input("Olá, qual o seu nome? ")
-print("Okay! %s, eu escolhendo um número de 1 até 10. Você consegue adivinhar?")
+print("Okay! %s, eu escolhendo um número de 1 até 10. Você consegue adivinhar?" %seu_nome)
 
+numAdvinhado = randint(1, 10)
+numTentativas = 4
+
+for tentativa in range(0, numTentativas):
+    numEscolhido = int(input("Escolha um número: "))
+    if numEscolhido == numAdvinhado:
+        print("Você Acertou em %i tentativas! O numero era %i" %(numTentativas, numAdvinhado))
+        break
+    elif numEscolhido > numAdvinhado:
+        print("Escolha um número menor")
+    elif numEscolhido < numAdvinhado:
+        print("Escolha um numero maior")
+    else:
+        print("Você não adivinhou")
+
+print("obrigado por jogar ")
 
--- Jogguinhos/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-# --Importar da biblioteca random a funação que pega uma numero aleatorio
-from random import randint 
-# -- Inputs
-
-seu_nome = input("Olá, qual o seu nome? ")
-print("Okay! %s, eu escolhendo um número de 1 até 10. Você consegue adivinhar?")
-
